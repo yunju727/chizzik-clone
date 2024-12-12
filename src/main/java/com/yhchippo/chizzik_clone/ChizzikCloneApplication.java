a7350f9f84a49727b522662efff7b5488c913c19
yunju727 <80828786+yunju727@users.noreply.github.com>
2024-12-12 21:59:12 +0900

    Initial commit

---
+++ src/main/java/com/yhchippo/chizzik_clone/ChizzikCloneApplication.java
@@ -0,0 +1,13 @@
+package com.yhchippo.chizzik_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChizzikCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChizzikCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhchippo/chizzik_clone/ChizzikCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhchippo.chizzik_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChizzikCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
